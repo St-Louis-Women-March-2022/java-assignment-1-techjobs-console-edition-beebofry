830c802ff4bd1e110d5c2a2299ce6a0598f41dc5
Kim Lee <you@example.com>
2022-08-21 16:01:24 -0500

    printJobs implementation completed

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -10,7 +10,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -112,14 +112,22 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() > 0) {
+            for (HashMap jobs : someJobs) {
+                System.out.println("\n*****");
+                jobs.forEach((key, value) -> System.out.println(key + ": " + value)); //IJ wouldn't accept Map.Entry and recommended forEach method
+                System.out.println("*****");
+            }
+        } else {
+            System.out.print("No Results");
+        }
     }
 }
+
