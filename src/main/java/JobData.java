ac7d16e7d47dee0c1d0506a9aa0c187b5fc6f977
Kim Lee <you@example.com>
2022-08-22 08:08:05 -0500

    find by value method and case insensitivity feature added

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -5,10 +5,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -37,7 +34,7 @@ public class JobData {
         for (HashMap<String, String> row : allJobs) {
             String aValue = row.get(field);
 
-            if (!values.contains(aValue)) {
+            if (!values.contains(aValue.toLowerCase())) {
                 values.add(aValue);
             }
         }
@@ -77,7 +74,7 @@ public class JobData {
 
         for (HashMap<String, String> row : allJobs) {
 
-            String aValue = row.get(column);
+            String aValue = row.get(column).toLowerCase();
 
             if (aValue.contains(value)) {
                 jobs.add(row);
@@ -99,7 +96,18 @@ public class JobData {
         loadData();
 
         // TODO - implement this method
-        return null;
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> job : allJobs) {
+            for (Map.Entry<String, String> search : job.entrySet()) {
+                String key = search.getKey().toLowerCase();
+                String aValue = search.getValue().toLowerCase();
+                if (key.contains(value) || aValue.contains(value)) {
+                    jobs.add(job);
+                }
+            }
+
+        } return jobs;
     }
 
     /**
